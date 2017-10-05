53adba93f73c8a3eabbdc083e110dee7678b9040
NBeorn <nmbeorn@gmail.com>
2017-10-05 15:13:45 -0400

    end of day

---
+++ Week2_Day4/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day4
+{
+    class Cat
+    {
+        //States
+        //fields
+        private string name;
+
+        //properties -- in order to interact with fields, must go through properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //Behaviors
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor - has no parameters in the parentheses
+        }
+    }
+}
